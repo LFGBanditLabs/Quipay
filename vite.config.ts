70715d9fbe9df1e794cc428fd39ba3c2161a5e19
brightpixel <brightekezie483@gmail.com>
2026-08-21 19:38:04 +0100

    fix: increase workbox precache limit to 5MiB for large bundle

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(() => {
         registerType: "autoUpdate",
         workbox: {
           globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-          maximumFileSizeToCacheInBytes: 4194304, // 4MiB
+          maximumFileSizeToCacheInBytes: 5242880, // 5MiB
           runtimeCaching: [
             {
               urlPattern: /^https:\/\/horizon-testnet\.stellar\.org\/.*/i,
